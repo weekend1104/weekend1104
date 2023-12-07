fb885904c5ad9e7fbcc337637a8110185f53b93d
weekend1104 <120648624@qq.com>
2023-12-07 17:48:41 +0800

    111

--- DevPY/InsertApp/view.py
+++ DevPY/InsertApp/view.py
@@ -37,35 +37,52 @@ def index():
     return render_template('/view/index.html',servername= server_name,form=form)
 
 
-@app.route('/devinfo',methods=["get","post"])
+@app.route('/devinfo',methods=["GET","POST"])
 def devinfo():
     form = Forms.DevDataIn()
-    DataResult = db.session.query(ClDevice).all()
-    if form.validate_on_submit():
-        if form.savepush.data:
-            devdata = ClDevice(deviceid=form.deviceid.data,
-                                 devname=form.name.data,
-                                 type=form.type.data,
-                                 serviceid=form.serviceid.data,
-                                 charid=form.charid.data,
-                                 startSampling=form.startSampling.data,
-                                 endSampling=form.endSampling.data
-                                 )
-            try:
-                db.session.add(devdata)
-                db.session.commit()
-                flash("提交完成！！！")
-            except BaseException:
-                db.session.rollback()
-                flash("提交失败！！！")
-        return redirect(url_for('devinfo'))
+    # 字符串+0 可以让字符串按照数字排序
+    DataResult = db.session.query(ClDevice).order_by(ClDevice.deviceid+0).all()
+    if request.method=="POST":
+        if form.validate_on_submit():
+            if form.savepush.data:
+                devdata = ClDevice(deviceid=form.deviceid.data,
+                                    devname=form.name.data,
+                                    type=form.type.data,
+                                    serviceid=form.serviceid.data,
+                                    charid=form.charid.data,
+                                    startSampling=form.startSampling.data,
+                                    endSampling=form.endSampling.data
+                                    )
+                try:
+                    db.session.add(devdata)
+                    db.session.commit()
+                    flash("提交完成！！！")
+                except BaseException:
+                    db.session.rollback()
+                    flash("提交失败！！！")
+            return redirect(url_for('devinfo'))
     return render_template('/view/devinfo.html',form=form,devdata=DataResult)
 
 
 @app.route('/deldev',methods=["POST"])
-def delinfo():
+def deldev():
     if request.method=="POST":
         devdata = request.get_json()
         devid = devdata.get('devid')
-        print(devid)
-        return 'success'
+        dataresult = ClDevice.query.filter_by(deviceid=devid).first()
+        try:
+            db.session.delete(dataresult)
+            db.session.commit()
+            responsejson = {
+                "message":'Data is delete!',
+                "code":200
+            }
+            response = make_response(responsejson)
+        except BaseException:
+            db.session.rollback()
+            responsejson = {
+                "message":'Data Roll Back!',
+                "code":500
+            }
+            response = make_response(responsejson)
+        return response
